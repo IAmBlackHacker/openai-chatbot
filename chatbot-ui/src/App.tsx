31ba855b0da4f77226ec217078db923e0395b4f1
Lokesh <24620094+IAmBlackHacker@users.noreply.github.com>
2024-09-22 13:50:57 +0530

    UI

--- chatbot-ui/src/App.tsx
+++ chatbot-ui/src/App.tsx
@@ -1,33 +1,65 @@
-import React from 'react';
+import React, {Dispatch, SetStateAction, useState} from 'react';
 import './App.css';
 import {FormControl, InputGroup, Form, Button} from "react-bootstrap";
 
+interface MessageInterface {
+    name: string;
+    message: string;
+}
+
+function OnFormSubmit(event: any, setMessage: Dispatch<SetStateAction<MessageInterface[]>>) {
+    event.preventDefault();
+    setMessage(oldMessages => [...oldMessages, {
+        name: "User",
+        message: event.target.query.value
+    }]);
+    return false;
+}
+
 function App() {
-  return (
-    <div className="App d-flex">
-        <div className={"container d-flex p-3"}>
-            <div className={"p-2 flex-fill d-flex bg-light border rounded"}>
-            <div className={"d-flex flex-column flex-fill overflow-hidden border rounded"}>
-                <div className={"flex-grow-1"}>
-                    <div className={"bg-dark text-white p-2 text-center"}>
-                        Chatbot UI
+    const [messages, setMessages] = useState<MessageInterface[]>([]);
+    const [inputQuery, SetInputQuery] = useState("");
+
+    return (
+        <div className="App d-flex">
+            <div className={"container d-flex p-3"}>
+                <div className={"p-2 flex-fill d-flex bg-light border rounded"}>
+                    <div className={"d-flex flex-column flex-fill overflow-hidden border rounded"}>
+                        <div className={"flex-grow-1"}>
+                            <div className={"bg-dark text-white p-2 text-center"}>
+                                Chatbot UI
+                            </div>
+                            <div className={"p-2"}>
+                                {
+                                    messages.map((message, index) => <div key={"message_" + index} className={"bg-success p-2 my-2 text-white rounded"}>
+                                        <p className={"m-0 fw-bold"}>{message.name}</p>
+                                        <p className={"m-0"}>{message.message}</p>
+                                    </div>)
+                                }
+                            </div>
+                        </div>
+                        <Form onSubmit={(event) => {
+                            SetInputQuery("");
+                            OnFormSubmit(event, setMessages);
+                        }}>
+                        <InputGroup className={""}>
+                            <Form.Control
+                                className={"shadow-none"}
+                                placeholder="User Query"
+                                name={"query"}
+                                value={inputQuery}
+                                onChange={(event) => {SetInputQuery(event.target.value);}}
+                            />
+                            <Button variant="primary" id="button-addon2">
+                                Send
+                            </Button>
+                        </InputGroup>
+                        </Form>
                     </div>
                 </div>
-                <InputGroup className="">
-                    <Form.Control
-                        placeholder="User Query"
-                        aria-label="query"
-                        aria-describedby="basic-addon1"
-                    />
-                    <Button variant="primary" id="button-addon2">
-                        Send
-                    </Button>
-                </InputGroup>
-            </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
